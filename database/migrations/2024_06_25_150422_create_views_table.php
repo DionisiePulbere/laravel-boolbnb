8905ab4afbabf08ea9540271ea1e62991c269589
NeilMagon <neil.magon97@gmail.com>
2024-06-25 17:14:17 +0200

    feat: add table view and relation with apartment

---
+++ database/migrations/2024_06_25_150422_create_views_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('apartment_id')->unsigned();
+            $table->integer('address_ip')->unsigned();
+            $table->dateTime('date_visit');
+            $table->timestamps();
+
+            $table->foreign('apartment_id')
+            ->references('id')
+            ->on('apartments')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('views');
+    }
+};
