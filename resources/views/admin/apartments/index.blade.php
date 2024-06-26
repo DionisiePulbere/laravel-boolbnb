1608245c5c806cb82c664fa8df59e69b894c451c
DamianoSpampinato <ghgg585@gmail.com>
2024-06-26 12:34:59 +0200

    Feat:add index init

---
+++ resources/views/admin/apartments/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<table class="table table-striped table-hover">
+    <th></th>
+        @foreach ($apartments as $apartment)
+        
+        @endforeach
+    </table>
+        
+
+@endsection
