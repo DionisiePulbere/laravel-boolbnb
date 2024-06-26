38be7e8b486b0804381969c93dfb5be4b9ea050a
iTsGiuse <giuse.amato@icloud.com>
2024-06-26 12:01:57 +0200

    fix

--- database/seeders/SponsorshipsTableSeeder.php
+++ database/seeders/SponsorshipsTableSeeder.php
@@ -17,21 +17,21 @@ class SponsorshipsTableSeeder extends Seeder
     public function run()
     {
         $newSponsor1= new Sponsorship();
-        $newSponsor1 -> type = 'one day';
+        $newSponsor1 -> type = 'One day';
         $newSponsor1 -> price = 2.99;
-        $newSponsor1 -> time = 24;
+        $newSponsor1 -> duration = '24:00:00';
         $newSponsor1->save();
 
         $newSponsor2= new Sponsorship();
-        $newSponsor2 -> type = 'three days';
+        $newSponsor2 -> type = 'Three days';
         $newSponsor2 -> price = 5.99;
-        $newSponsor2 -> time = 72;
+        $newSponsor2 -> duration = '72:00:00';
         $newSponsor2->save();
 
         $newSponsor3= new Sponsorship();
-        $newSponsor3 -> type = 'one week';
+        $newSponsor3 -> type = 'Six days';
         $newSponsor3 -> price = 9.99;
-        $newSponsor3 -> time = 144;
+        $newSponsor3 -> duration = '144:00:00';
         $newSponsor3->save();
         
     }
