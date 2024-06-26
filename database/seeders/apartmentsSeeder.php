e7ee703135bfd5e66e2f650efb6f323521d077f0
DamianoSpampinato <ghgg585@gmail.com>
2024-06-26 12:01:21 +0200

    Feat:add Appartment seeder on DatabaseSeeder

---
+++ database/seeders/apartmentsSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Apartment;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class apartmentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<10 ; $i++){
+            $newApartment = new Apartment();
+            $newApartment->title = $faker->sentence(3);
+            $newApartment->visibility = 1;
+            $newApartment->thumb = 'https://picsum.photos/200/300';
+            $newApartment->description = $faker->sentence(10);
+            $newApartment->price =$faker->randomFloat(2, 100, 999) ;
+            $newApartment->number_of_room =$faker->randomDigit;
+            $newApartment->number_of_bed =$faker->randomDigit;
+            $newApartment->number_of_bath =$faker->randomDigit;
+            $newApartment->square_meters =$faker->numberBetween(50, 250);
+            $newApartment->latitude =$faker->latitude($min = -90, $max = 90);
+            $newApartment->longitude =$faker->longitude($min = -90, $max = 90);
+            $newApartment->address =$faker->streetAddress();
+            $newApartment->save();
+
+
+
+        }
+    }
+}
