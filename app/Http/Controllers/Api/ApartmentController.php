af2e31aa79d1f3f056b97d22aa7465debee1f5a5
DamianoSpampinato <ghgg585@gmail.com>
2024-06-27 16:42:48 +0200

    feat add init api

---
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Apartment;
+
+class ApartmentController extends Controller
+{
+    public function index (){
+        $apartments = Apartment::all();
+        return response()->json([
+            'success'=> true,
+            'results'=> $apartments
+        ]);
+    }
+
+    public function show($slug){
+        $apartment= Apartment::where('slug' ,'=', $slug)->first();
+        if($apartment){
+            return response()->json([
+                'success' => true,
+                'results' => $apartment
+            ]);
+
+        } else{
+            return response()->json([
+                'success' => false,
+                'error' => 'No Apartment found'
+            ]);
+        }
+        
+    }
+}
