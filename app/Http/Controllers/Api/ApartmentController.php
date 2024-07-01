0941b0a618c810d55f8d6e359659de469592f9fb
DamianoSpampinato <ghgg585@gmail.com>
2024-07-01 22:56:16 +0200

    feat:add api image response

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Apartment;
 
+
 class ApartmentController extends Controller
 {
     public function index (){
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApartmentController;
-
+use App\Http\Controllers\Api\ImageController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::get('/apartments', [ApartmentController::class, 'index']);
 Route::get('/apartments/{slug}', [ApartmentController::class, 'show']);
+Route::get('/apartment_image/{filename}', [imageController::class, 'show']);
+
