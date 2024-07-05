0fb7400657333da3dd708fbd2f8138d9ff2218e4
NeilMagon <neil.magon97@gmail.com>
2024-07-05 12:31:10 +0200

    fix

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -16,7 +16,7 @@ class PaymentController extends Controller
             'environment' => env('BRAINTREE_ENV'),
             'merchantId' => env('BRAINTREE_MERCHANT_ID'),
             'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
-            'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
+            'privateKey' => env('BRAINTREE_PRIVATE_KEY'), 
         ]);
     }
 
