a486069d69f73d32f15ce85e8c2675d578430f84
iTsGiuse <giuse.amato@icloud.com>
2024-07-05 12:17:32 +0200

    mezzo fix

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Message;
+use App\Models\Sponsorship;
+use App\Models\View;
 use Illuminate\Http\Request;
 use illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index() {
         $user = Auth::user();
-        
+        $totalSponsors = Sponsorship::count();
+        $totalViews = View::count();
+        $totalMessages = Message::count();
 
         $data = [
-            'user' => $user
+            'user' => $user,
+            'totalSponsors' => $totalSponsors,
+            'totalViews' => $totalViews,
+            'totalMessages' => $totalMessages,
         ];
 
         return view('admin.dashboard', $data);
